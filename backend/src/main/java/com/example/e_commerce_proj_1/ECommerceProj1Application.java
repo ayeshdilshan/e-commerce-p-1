a290a5f2db9c17f153ee76451b4573c050f7220f
ayesh <ayeshjayasundara0725@gmail.com>
2024-10-26 17:19:28 +0530

    add frontend and backend

---
+++ backend/src/main/java/com/example/e_commerce_proj_1/ECommerceProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_proj_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProj1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/e_commerce_proj_1/ECommerceProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce_proj_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
